e1171f3400953ac18aa934b37949dba3cac784af
bwexy <bridgetwexler1202@gmail.com>
2024-09-27 11:05:53 -0400

    Update Item.java
    I forogt to commit this with the inventory

--- AtoranProject/src/inventory/Item.java
+++ AtoranProject/src/inventory/Item.java
@@ -1,5 +1,30 @@
 package inventory;
 
 public class Item {
+	
+		
+		    private String itemName;
+		    private String itemdescription; //description will display on screen
+		    public Item(String name, String description) {
+		        this.itemName = name;
+		        this.itemdescription = description;
+		    }
 
-}
+	
+		    public String getName() {
+		        return itemName;
+		    }
+
+		    public String getDescription() {
+		        return itemdescription;
+		    }
+		   
+		    public void useItem() {
+		        System.out.println("Using the item: " + itemName); //overwritten 
+		    }
+
+		    @Override
+		    public String toString() {
+		        return "Item Name: " + itemName + ", Item Description: " + itemdescription; //overwrite to string
+		    }
+		}
